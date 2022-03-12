839b8420c0dbea5b7855baecfe5f1d3c8f7fd71a
PairApp <79168736+PairApp@users.noreply.github.com>
2022-03-11 20:51:14 -0800

    Update 2-useState-basics.js

--- src/tutorial/1-useState/final/2-useState-basics.js
+++ src/tutorial/1-useState/final/2-useState-basics.js
@@ -6,9 +6,7 @@ import React, { useState } from 'react';
 
 const UseStateBasics = () => {
   // console.log(useState());
-  // const value = useState()[0];
-  // const handler = useState()[1];
-  // console.log(value, handler);
+  
 
   const [text, setText] = useState('random title');
   const handleClick = () => {
